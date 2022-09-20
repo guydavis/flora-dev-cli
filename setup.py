a0dff73db7e3c45f8b91790ad124b493a4745347
Guy Davis <guydavis.ca@gmail.com>
2022-09-20 09:32:54 -0600

    Setuptools to Chia dependency level.

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ if __name__ == '__main__':
         author_email='',
         description='',
         install_requires=[
-            'click~=8.1.3',
+            'click~=7.1.2',
             'chia-blockchain~=1.5.1',
             'setuptools~=65.3.0',
             'requests~=2.28.1'
